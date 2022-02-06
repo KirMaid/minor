101c3e778a3ee6225f140a5941df2bd1f7fd7de5
KirMaid <ialavash228@gmail.com>
2022-02-06 14:33:19 +0300

    init:start project

---
+++ src/components/AddPost.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form @submit.prevent>
+      <input type="text" class="form-control" v-model="post.title" />
+      <input type="text" class="form-control" v-model="post.text" />
+      <input class="form-control" type="file" id="formFile" />
+      <common-btn style="margin-bottom: 1rem" @click="addPost">Добавить пост</common-btn>
+    </form>
+  </div>
+</template>
+
+<script>
+// import CommonButton from "./UI/CommonButton";
+export default {
+  components: {},
+  data() {
+    return {
+      post: {
+      },
+    };
+  },
+  methods: {
+    addPost() {
+      this.$emit("add", this.post);
+      this.post.title = "";
+      this.post.text = "";
+    },
+  },
+};
+</script>
+<style>
+input {
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+.form-control {
+  margin-bottom: 1rem;
+}
+</style>
---
+++ src/components/PostsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post_item v-for="post in posts"  :key="post.id" :post="post"></Post_item >
+  </div>
+</template>
+
+<script>
+import Post_item from "./Post_item";
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    Post_item,
+  },
+};
+</script>
+
+<style>
+</style>
